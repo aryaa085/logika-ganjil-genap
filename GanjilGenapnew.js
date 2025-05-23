bacb5856eabcfffb2b2788bd3cd582e0a962935e
aryaa085 <kurniawanarya.2023@student.uny.ac.id>
2025-05-23 13:45:46 +0700

    Add files via upload

---
+++ GanjilGenapnew.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+
+function cekGanjilGenap(angka) {
+  if (angka % 2 === 0) {
+    console.log(`${angka} adalah bilangan genap.`);
+  } else {
+    console.log(`${angka} adalah bilangan ganjil.`);
+  }
+}
+
+// Buat interface untuk membaca input dari user
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Minta input dari user
+rl.question('Masukkan angka : ', (input) => {
+  const angka = parseInt(input);
+
+  if (!isNaN(angka)) {
+    cekGanjilGenap(angka);
+  } else {
+    console.log("bukan angka");
+  }
+
+  rl.close(); // tutup input setelah selesai
+});
